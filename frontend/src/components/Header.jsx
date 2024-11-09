52938ad7895d67f891b12eeceff6a2c3a897ce2d
viLam11 <ngoc.huynhcs2212257@hcmut.edu.com>
2024-11-09 15:14:05 +0700

    wait FE

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+export default function Header() {
+    return( 
+        <header className="h-20 bg-black text-white w-full flex justify-between items-center">
+           <span className="px-6 text-2xl" >EXCLUSIVE</span>
+
+            <span className="absolute left-1/2 transform -translate-x-1/2 space-x-4 text-xl ">
+                <a>Homepage</a>
+                <a className="underline decoration-solid">Login </a>
+            </span>
+        </header>
+    )
+}
