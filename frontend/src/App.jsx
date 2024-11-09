4ae916185515efb98c7da5cf73130b8b1cb79a62
viLam11 <ngoc.huynhcs2212257@hcmut.edu.com>
2024-11-09 21:41:48 +0700

    product managemenet page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import AuthProvider from "./AuthProvider";
 import Login from "./pages/Login";
 import ProductsManagement from "./pages/ProductsManagement";
+import NewProduct from "./pages/NewProduct";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/product-manage" element={<ProductsManagement />} />
+          <Route path="/product-new"element={<NewProduct />} />
         </Routes>
       </AuthProvider>
     </Router>
