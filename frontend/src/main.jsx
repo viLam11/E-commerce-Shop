97014fbdb6107ddb0c3d305f6db937890aabd392
duongCyberclone123 <120347988+duongCyberclone123@users.noreply.github.com>
2024-11-09 22:04:53 +0700

    Add files via upload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import PageComponent from './components/customerHomePage.jsx'
+import App from './components/App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <PageComponent />
+);
+// reportWebVitals();
