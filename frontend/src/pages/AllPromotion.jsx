a7c359ab56663d6e9dc012eefd09a0e1a9e37f84
viLam11 <ngoc.huynhcs2212257@hcmut.edu.com>
2024-12-06 16:18:33 +0700

    check

--- frontend/src/pages/AllPromotion.jsx
+++ frontend/src/pages/AllPromotion.jsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function AllPromotion() {
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
     const [promotionData, setPromotionData] = useState([]);
 
     useEffect(() => {
