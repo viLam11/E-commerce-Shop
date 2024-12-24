c61f0e027cf9a422ac5265578040787aa0d28e92
viLam11 <ngoc.huynhcs2212257@hcmut.edu.com>
2024-12-24 13:54:10 +0700

    delete FE

--- frontend/src/components/CategoryCard.jsx
+++
@@ -1,7 +0,0 @@
-export default function CatogoryCard({catName}) {
-    return(
-        <div className="w-full rounded-lg bg-white mx-auto">
-            <div className="text-center">{catName}</div>
-        </div>
-    )
-}
